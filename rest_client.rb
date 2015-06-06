48713a7116a1ed0335bad7c7b681bf192bd1e040
John Epperson <j.epperson@verizon.net>
2015-06-05 20:15:20 -0400

    Change RestClient tester to run 1000 requests randomly

--- rest_client.rb
+++ rest_client.rb
@@ -1,5 +1,6 @@
 require 'rest-client'
 require 'pry'
+require 'benchmark'
 
 class Leaderboard
   def self.index(size = 10, offset = 0)
@@ -24,6 +25,36 @@ class Leaderboard
   end
 end
 
-binding.pry
+def make_random_request
+  choices = [:index, :create, :destroy, :show]
+  random_choice = rand(0..3)
+  send(:"make_#{choices[random_choice]}_request")
+end
+
+def make_index_request
+  Leaderboard.index(rand(1..10), rand(0..9))
+rescue RestClient::ResourceNotFound
+  true
+end
+
+def make_create_request
+  Leaderboard.create("Name#{rand(1..10)}", rand(1..10))
+end
+
+def make_destroy_request
+  Leaderboard.destroy("Name#{rand(1..10)}")
+rescue RestClient::ResourceNotFound
+  true
+end
+
+def make_show_request
+  Leaderboard.show("Name#{rand(1..10)}")
+rescue RestClient::ResourceNotFound
+  true
+end
+
+Benchmark.bm do |benchmark|
+  benchmark.report { 1000.times { |_| make_random_request } }
+end
 
 sleep(1)
